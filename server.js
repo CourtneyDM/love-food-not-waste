eda9b93b2f0469f68831f88c57435bf102f3de0f
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-07-08 03:27:25 -0500

    included environment variables for hosting on Heroku

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // Import Dependencies
-require( 'dotenv' ).config();
-
 const bodyParser = require( 'body-parser' );
 const express = require( 'express' );
+const mongoose = require( 'mongoose' );
+const routes = require( './routes' );
 
 // Configure server to use Express
 const app = express();
@@ -14,9 +14,17 @@ const PORT = process.env.PORT || 3001;
 app.use( bodyParser.urlencoded( { extended: true } ) );
 app.use( bodyParser.json() );
 
+// Serve Static Pages on Heroku
+if ( process.env.NODE_ENV === 'production' ) {
+    app.use( express.static( 'client/build' ) );
+}
+
+// Configure routes
+app.use( routes );
+
+// Setup connection to MongoDB
+// mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/wasteNot', { useNewUrlParser: true } );
+mongoose.connect( 'mongodb://localhost:27017/wasteNot', { useNewUrlParser: true } );
+
 // Start Server...
 app.listen( PORT, () => console.log( `Express server listening on PORT ${PORT}` ) );
-
-console.log( process.env.REACT_APP_X_MASHAPE_KEY );
-
-// TODO: Setup server to connect to database - MySQL? Mongoose?
