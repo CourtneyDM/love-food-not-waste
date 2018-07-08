fcd75d9022c2c9176b8f7964456f650d9be119b7
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-07-08 03:32:40 -0500

    API calls to Nutritionix API and database

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,29 +1,48 @@
-require( 'dotenv' ).config();
-const unirest = require( 'unirest' );
+import axios from 'axios';
 
-const X_MASHAPE_KEY = 'jQfnjagUlOmsh6jakRb6pEeFbK99p1ebYpGjsnhQRHifZupNG2';
+export default {
 
-const SPOONACULAR_HOST = 'spoonacular-recipe-food-nutrition-v1.p.mashape.com';
+    // Query Nutritionix database for food item
+    getFoodDetails: query => {
+        const X_APP_KEY = 'e52dae96ffda6839ead180a00dced399';
+        const X_APP_ID = '0d95c6f7';
+        return axios( {
+            method: 'get',
+            url: `https://trackapi.nutritionix.com/v2/search/instant?query=${query}`,
+            headers: {
+                'x-app-key': X_APP_KEY,
+                'x-app-id': X_APP_ID
+            }
+        } );
+    },
 
-const query = 'big mac';
-const UPC = '013409515372';
+    // TODO: Query Nutritionix database for upc item 
+    getBarcodeDetails: query => {
+        const X_APP_KEY = 'e52dae96ffda6839ead180a00dced399';
+        const X_APP_ID = '0d95c6f7';
+        return axios( {
+            method: 'get',
+            url: `https://nutritionix-api.p.mashape.com/v1_1/item?upc=${query}`,
+            headers: {
+                'x-app-key': X_APP_KEY,
+                'x-app-id': X_APP_ID
+            }
+        } );
+    },
 
-const NUTRITIONIX_HOST = 'nutritionix-api.p.mashape.com';
+    // Save a food item to inventory
+    saveFoodItem: foodData => {
+        console.log( 'saving to inventory...' );
+        return axios.post( '/api/inventory', foodData );
+    },
 
+    getInventory: () => {
+        console.log( 'getting inventory...' );
+        return axios.get('/api/inventory');
+    },
 
-
-// Search for a given food item
-// unirest.get( `https://trackapi.nutritionix.com/v2/search/instant?query=${query}` )
-//     .header( 'x-app-key', 'e52dae96ffda6839ead180a00dced399' )
-//     .header( 'x-app-id', '0d95c6f7' )
-//     .end( function ( response ) {
-//         // console.log( result.status, result.headers, result.body );
-//         console.log( response.body /*response.data, response.body */ );
-//     } );
-
-unirest.get( `https://nutritionix-api.p.mashape.com/v1_1/item?upc=${UPC}` )
-    .header( "X-Mashape-Key", X_MASHAPE_KEY )
-    .header( "X-Mashape-Host", NUTRITIONIX_HOST )
-    .end( function ( result ) {
-        console.log( result.status, result.headers, result.body );
-    } );
+    deleteFoodItem: id => {
+        console.log( id );
+        return axios.delete('/api/inventory/' + id);
+    }
+}
