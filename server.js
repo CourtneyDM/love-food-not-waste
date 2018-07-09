fac3c6a074f3fc6a9443fc174c3a66e0010d83c6
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-07-08 20:46:56 -0500

    Added comments

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 // Configure PORT
 const PORT = process.env.PORT || 3001;
 
-// Setup connection to MongoDB
+// Setup connection to MongoDB for Heroku
 const databaseUri = 'mongodb://localhost:27017/wasteNot';
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://heroku_xsqdq0bn:ovl3l58hlmo7tt72lo4sdm8gnm@ds129811.mlab.com:29811/heroku_xsqdq0bn';
 
@@ -21,6 +21,7 @@ else {
     mongoose.connect( databaseUri, { useNewUrlParser: true } );
 }
 
+// Validate connection to database
 const db = mongoose.connection;
 db.on( 'error', err => console.log( `Mongoose error: ${err}` ) );
 db.once( 'open', () => console.log( 'Mongoose connection successful.' ) );
@@ -31,7 +32,7 @@ app.use( bodyParser.json() );
 
 // Serve Static Pages on Heroku
 if ( process.env.NODE_ENV === 'production' ) {
-    app.use( express.static( 'client/build' ) );
+    app.use( express.static( 'client/public' ) );
 }
 
 // Configure routes
