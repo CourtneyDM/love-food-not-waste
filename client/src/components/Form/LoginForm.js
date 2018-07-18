939019289edaff242d01feaa63bd870be9d60b4c
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-07-18 08:53:46 -0500

    added dashboard component for redirect; saving data to window session storage

--- client/src/components/Form/LoginForm.js
+++ client/src/components/Form/LoginForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Input, Button } from '../Form';
+import Dashboard from '../../pages/Landing';
 import axios from 'axios';
 import './Form.css';
 
@@ -8,66 +9,94 @@ export class LoginForm extends Component {
         super( props );
         this.state = {
             email: '',
-            password: ''
+            password: '',
+            sessionID: ''
         };
         this.handleClick = this.handleClick.bind( this );
         this.handleInputChange = this.handleInputChange.bind( this );
     }
 
+    // Handle the button click event
     handleClick = event => {
         event.preventDefault();
         const user = {
-            email: this.state.email,
-            password: this.state.password
+            email: this.state.email.trim(),
+            password: this.state.password.trim()
         };
-        return axios( {
-            method: 'POST',
-            url: "/login",
-            data: {
-                email: this.state.email,
-                password: this.state.password
-            }
-        } ).then( res => console.log( res ) ).catch( err => { throw err } );
-        // axios.post( '/login', user )
-        //     .then( res => console.log( res ) )
-        //     .catch( err => { throw err; } );
-        // TODO: Create functionality to handle form submission
+
+        return this.getUserData( user );
     }
 
+    // Handle input change event for input fields
     handleInputChange = event => {
         event.preventDefault();
         const { name, value } = event.target;
         this.setState( { [ name ]: value } );
     }
 
+    // Get the user data from back-end server
+    getUserData = user => {
+        axios( {
+            method: 'POST',
+            url: "/login",
+            data: {
+                email: user.email.trim(),
+                password: user.password.trim()
+            }
+        } ).then( res => {
+            console.log( res );
+            if ( res.data === 'Invalid password.' ) {
+                return alert( 'You have entered the wrong password.' );
+            }
+            else if ( res.data === 'Invalid username.' ) {
+                return alert( 'You have entered the wrong username.' );
+            } else if ( res.data === 'Missing credentials' ) {
+                return alert( 'Please check your submission and try again.' );
+            }
+            else {
+                window.sessionStorage.setItem( "fullname", res.data.fullname );
+                window.sessionStorage.setItem( "email", res.data.email );
+                window.sessionStorage.setItem( "sessionID", res.data.sessionID );
+            }
+        } ).catch( err => { throw err } );
+    }
+
     render() {
         return (
-            <React.Fragment>
-                <h2>Account Login</h2>
-                <div className='signup'>
-                    <div className='form-group'>
-                        <Input
-                            label='Email'
-                            name='email'
-                            type='text'
-                            className='form-control'
-                            onChange={ this.handleInputChange }
-                        />
-                        <Input
-                            label='Password'
-                            name='password'
-                            type='password'
-                            className='form-control'
-                            onChange={ this.handleInputChange }
-                        />
-                        <Button
-                            onClick={ this.handleClick }
-                            text='Login'
-                            className='btn btn-success'
-                        />
+            ( window.sessionStorage ?
+                ( <React.Fragment>
+                    { window.sessionStorage.setItem( 'username', this.state.email ) }
+                    { window.sessionStorage.setItem( 'sessionID', this.state.sessionID ) }
+                    <Dashboard />
+                </React.Fragment>
+                )
+                :
+                ( <React.Fragment>
+                    <h2>Account Login</h2>
+                    <div className='signup'>
+                        <div className='form-group'>
+                            <Input
+                                label='Email'
+                                name='email'
+                                type='text'
+                                className='form-control'
+                                onChange={ this.handleInputChange }
+                            />
+                            <Input
+                                label='Password'
+                                name='password'
+                                type='password'
+                                className='form-control'
+                                onChange={ this.handleInputChange }
+                            />
+                            <Button
+                                onClick={ this.handleClick }
+                                text='Login'
+                                className='btn btn-success'
+                            />
+                        </div>
                     </div>
-                </div>
-            </React.Fragment>
+                </React.Fragment> ) )
         );
     }
 }
