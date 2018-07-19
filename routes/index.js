040598cb908dc7e352774ab2674b6c10274b6b7d
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-07-19 17:41:13 -0500

    testing for heroku

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const path = require( 'path' );
 const router = require( 'express' ).Router();
 const apiRoutes = require( './api' );
-const userRoutes = require( './auth/userRoutes' );
+// const userRoutes = require( './auth/userRoutes' );
 
 // API Routes
 router.use( '/api', apiRoutes );
