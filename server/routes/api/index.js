7c864fe88a6ebf69df3e436e7bc9585285c4f142
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-08-03 17:31:54 -0500

    added static routes for React app, updated start script and added environment PORT to server

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -17,5 +17,10 @@ router.use( '/inventory', inventoryRoutes );
 // Routes for Saved Users -> /api/users
 router.use( '/users', userRoutes );
 
+// If no API routes are hit, send the React app pages
+router.use( function ( req, res ) {
+    res.sendFile( path.join( __dirname, '../client/build/index.html' ) );
+} );
+
 // Export Router Routes
 module.exports = router;
