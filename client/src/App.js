c2ff2fdbed063e06cdc62da162ba3e1aa364d6b5
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-08-04 16:46:47 -0500

    Courtney (#8)
    * updated routes and implemented willMount lifecycle method
    
    * added props.match.url path for login component
    
    * reverting changes to routing
    
    * testing match.params.login for Login Component
    
    * updating routes for testing on Heroku
    
    * updates

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,19 @@ export default class App extends Component {
                     <Jumbotron />
                     <div className='siteContent'>
                         <Switch>
+<<<<<<< HEAD
+                            <Route exact path='/' component={ Home } />
+                            <Route path='/:login' component={ Login } />
+                            <Route path='/:signup' component={ Signup } />
+                            <Route path='/:dashboard' component={ Dashboard } />
+                            <Route path='/:AtHome/Inventory' component={ Inventory } />
+                            <Route path='/AtHome/Recipes' component={ Recipes } />
+                            <Route path='/Educate/FoodWaste' component={ FoodWaste } />
+                            <Route path='/Educate/Hunger' component={ Hunger } />
+                            <Route path='/GetInvolved/FoodSupplier' component={ FoodSupplier } />
+                            <Route path='/GetInvolved/Individual' component={ Individual } />
+                            <Route path='/GetInvolved/Resources' component={ Resources } />
+=======
                             <Route exact path={ '/' } component={ Home } />
                             <Route path={ `${match.params}/login` } component={ Login } />
                             <Route path={ `${match.params}/signup` } component={ Signup } />
@@ -30,6 +43,7 @@ export default class App extends Component {
                             <Route path={ '/GetInvolved/FoodSupplier' } component={ FoodSupplier } />
                             <Route path={ '/GetInvolved/Individual' } component={ Individual } />
                             <Route path={ '/GetInvolved/Resources' } component={ Resources } />
+>>>>>>> 3304935d8f77029f8f79d0678a7c04ad0a85a458
                             <Route component={ NoMatch } />
                         </Switch>
                     </div>
