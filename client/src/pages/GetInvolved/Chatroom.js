89e6f7e3f75fa9ccc200e680f7c9b0a8ff246984
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-08-04 21:36:23 -0500

    updated functionality to save messages to messageboard db

--- client/src/pages/GetInvolved/Chatroom.js
+++ client/src/pages/GetInvolved/Chatroom.js
@@ -21,6 +21,10 @@ class Chatroom extends React.Component {
         this.scrollToBot();
     }
 
+    componentDidUpdate() {
+        this.scrollToBot();
+    }
+
     // Handle input field changes
     handleInputChange = event => {
         const { name, value } = event.target;
@@ -31,63 +35,64 @@ class Chatroom extends React.Component {
     handleClick = event => {
         event.preventDefault();
         alert( this.state.itemName );
+        this.setState( {
+            message: this.state.message
+        } );
+        console.log( `Message submitted: ${message}` );
+        return this.saveChat( message );
     }
 
-    componentDidUpdate() {
-        this.scrollToBot();
-    }
+
 
     scrollToBot() {
         ReactDOM.findDOMNode( this.refs.chats ).scrollTop = ReactDOM.findDOMNode( this.refs.chats ).scrollHeight;
     }
 
     getChat = () => {
-        API.getChat()
-
-            .then( results => {
-                this.setState( {
-                    chats: results.data.data,
-                } );
-                console.log( this.state.chats );
-            } )
-            .catch( error => { throw error } );
-    }
-
-    // Save food item to database
-    saveChat = ( message ) => {
-        API.saveChat( message )
-            .then( console.log( message ) )
-            .catch( error => { throw error } );
-    }
-
-
-    saveChatClick = () => {
-        console.log( "Saving message:" + this.state.message )
-        this.saveChat( {
-            message: this.state.message
+        API.getChat().then( results => {
+            this.setState( {
+                chats: results.data.data,
+            } );
+            console.log( this.state.chats );
         } )
-
-        this.setState( {
-            message: ''
-        } );
-        this.getChat();
-
+            .catch( error => { throw error } );
     }
-    render() {
 
+    // Save recent post to chat database
+    saveChat = message => {
+        return API.saveChat( message )
+            .then( res => console.log( `saveChat response: ${res}` ) )
+            .catch( error => { throw error } );
+        // TODO: update chat array with response from API call - call getChat method
+    }
+
+    // saveChatClick = () => {
+    //     console.log( "Saving message:" + this.state.message )
+    //     this.saveChat( {
+    //         message: this.state.message
+    //     } )
+
+    //     this.setState( {
+    //         message: ''
+    //     } );
+    //     this.getChat();
+
+    // }
+    render() {
         return (
             <CardDeck>
                 <CardBasic
-
                     header='Chat with Locals Looking to Help in the Fight Against Food Waste'>
-
                     <div className='chatroom'>
-
                         <div className="chats" ref="chats">
-
-                            { this.state.chats.slice( 0, this.state.chats.length ).map( ( chat, index ) => {
-                                return <Message key={ index } date={ chat.date } chat={ chat.message } />
-                            } ) }
+                            { this.state.chats
+                                .slice( 0, this.state.chats.length )
+                                .map( ( chat, index ) => {
+                                    return <Message
+                                        key={ index }
+                                        date={ chat.date }
+                                        chat={ chat.message } />
+                                } ) }
                         </div>
                     </div>
                     <div className='chatForm'>
@@ -95,17 +100,16 @@ class Chatroom extends React.Component {
                             id="chatMessageInput"
                             name='message'
                             placeholder='Enter your message here'
-                            onChange={ this.handleInputChange }>
+                            onChange={ this.handleInputChange } >
+
                             <Button
                                 className='btn btn-search'
-                                text='Chat'
-                                onClick={ () => this.saveChatClick()
-                                }
-                            />
+                                text='Post Message'
+                                onClick={ this.handleClick } />
                         </Input>
                     </div>
                 </CardBasic>
-            </CardDeck>
+            </CardDeck >
         );
     }
 }
