87730bcdef5e628b47e2ca76318d562beadf1a1b
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-08-08 00:21:08 -0500

    Courtney (#16)
    * updated comments
    
    * updated controllers for CRUD; reconfigured DELETE route for inventory
    
    * modified axios API call to delete function
    
    * updated controllers for CRUD; reconfigured DELETE route for inventory
    
    * updated controllers for CRUD; reconfigured DELETE route for inventory
    
    * removed console.log comments
    
    * Removed blank spaces
    
    * included Google Maps
    
    * revert changes to Google Maps React component
    
    * removed script for Google Maps
    
    * removed script for DataTables
    
    * saved coordinates for resources in variables

--- client/src/components/Maps/MapComponent.js
+++ client/src/components/Maps/MapComponent.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { compose, withProps } from 'recompose';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
 
+const NTFB = { lat: 32.6889959, lng: -96.8927529 };
+const RFP = { lat: 32.8078581, lng: -96.8159636 };
+const PGP = { lat: 32.7347708, lng: -96.6798481 };
+const OCP = { lat: 32.7588686, lng: -96.7784619 };
+const CRCS = { lat: 32.7782536, lng: -96.7961399 };
+
 const MapComponent = compose(
     withProps( {
         googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyCYUCuWg5hDZOmPFegBDjf5rDE6SAqofMc&v=3.exp&libraries=geometry,drawing,places",
@@ -20,19 +26,19 @@ const MapComponent = compose(
                 position={ { lat: 32.7767, lng: -96.7970 } }
                 onClick={ props.onMarkerClick } /> }
         { props.isMarkerShown &&
-            <Marker position={ { lat: 32.6889959, lng: -96.8927529 } }
+            <Marker position={ NTFB }
                 onClick={ props.onMarkerClick } /> }
         { props.isMarkerShown &&
-            <Marker position={ { lat: 32.8078581, lng: -96.8159636 } }
+            <Marker position={ RFP }
                 onClick={ props.onMarkerClick } /> }
         { props.isMarkerShown &&
-            <Marker position={ { lat: 32.7347708, lng: -96.6798481 } }
+            <Marker position={ PGP }
                 onClick={ props.onMarkerClick } /> }
         { props.isMarkerShown &&
-            <Marker position={ { lat: 32.7588686, lng: -96.7784619 } }
+            <Marker position={ OCP }
                 onClick={ props.onMarkerClick } /> }
         { props.isMarkerShown &&
-            <Marker position={ { lat: 32.7782536, lng: -96.7961399 } }
+            <Marker position={ CRCS }
                 onClick={ props.onMarkerClick } /> }
     </GoogleMap>
 );
