765c1c4bbd672d5640e0bb270f57d85b14a222c7
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-08-11 14:45:47 -0500

    working on fixing Map Component

--- client/src/components/Maps/MapComponent.js
+++ client/src/components/Maps/MapComponent.js
@@ -8,40 +8,51 @@ const PGP = { lat: 32.7347708, lng: -96.6798481 };
 const OCP = { lat: 32.7588686, lng: -96.7784619 };
 const CRCS = { lat: 32.7782536, lng: -96.7961399 };
 
+// TODO: Finish this
+const pos = navigator.geolocation.getCurrentPosition( position => {
+    const pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+    }
+} );
+
 const MapComponent = compose(
     withProps( {
         googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyCYUCuWg5hDZOmPFegBDjf5rDE6SAqofMc&v=3.exp&libraries=geometry,drawing,places",
         loadingElement: <div style={ { height: `100%` } } />,
         containerElement: <div style={ { height: `400px` } } />,
         mapElement: <div style={ { height: `100%` } } />,
-    } ),
-    withScriptjs,
-    withGoogleMap
-)( props =>
-    <GoogleMap
-        defaultZoom={ 12 }
-        defaultCenter={ { lat: 32.7767, lng: -96.7970 } } >
-        { props.isMarkerShown &&
-            <Marker
-                labelAnchor={ `Dallas, Texas` }
-                position={ { lat: 32.7767, lng: -96.7970 } }
-                onClick={ props.onMarkerClick } /> }
-        { props.isMarkerShown &&
-            <Marker position={ NTFB }
-                onClick={ props.onMarkerClick } /> }
-        { props.isMarkerShown &&
-            <Marker position={ RFP }
-                onClick={ props.onMarkerClick } /> }
-        { props.isMarkerShown &&
-            <Marker position={ PGP }
-                onClick={ props.onMarkerClick } /> }
-        { props.isMarkerShown &&
-            <Marker position={ OCP }
-                onClick={ props.onMarkerClick } /> }
-        { props.isMarkerShown &&
-            <Marker position={ CRCS }
-                onClick={ props.onMarkerClick } /> }
-    </GoogleMap>
-);
+    } ), withScriptjs, withGoogleMap )( props =>
+        <GoogleMap
+            defaultZoom={ 12 }
+            defaultCenter={ { lat: 32.7767, lng: -96.7970 } } >
+            { props.isMarkerShown &&
+                <Marker
+                    // position={ { lat: 32.7767, lng: -96.7970 } }
+                    posititon={ navigator.geolocation.getCurrentPosition() }
+                    onClick={ props.onMarkerClick } /> }
+            { props.isMarkerShown &&
+                <Marker
+                    defaultTitle={ "North Texas Food Bank" }
+                    position={ NTFB }
+                    onClick={ props.onMarkerClick } /> }
+            { props.isMarkerShown &&
+                <Marker
+                    position={ RFP }
+                    onClick={ props.onMarkerClick } /> }
+            { props.isMarkerShown &&
+                <Marker
+                    position={ PGP }
+                    onClick={ props.onMarkerClick } /> }
+            { props.isMarkerShown &&
+                <Marker
+                    position={ OCP }
+                    onClick={ props.onMarkerClick } /> }
+            { props.isMarkerShown &&
+                <Marker
+                    position={ CRCS }
+                    onClick={ props.onMarkerClick } /> }
+        </GoogleMap>
+    );
 
 export default MapComponent;
