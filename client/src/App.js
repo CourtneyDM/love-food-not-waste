8972dcac71735e079474212826c68e1205160b6c
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-08-13 18:17:44 -0500

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -15,8 +15,8 @@ import history from './utils/Auth/history';
 
 const auth = new Auth();
 
-const handleAuthentication = (nextState, replace) => {
-    if (/access_token|id_token|error/.test(nextState.location.hash)) {
+const handleAuthentication = ( nextState, replace ) => {
+    if ( /access_token|id_token|error/.test( nextState.location.hash ) ) {
         auth.handleAuthentication();
     }
 }
@@ -25,44 +25,34 @@ const handleAuthentication = (nextState, replace) => {
 
 
 const App = () => (
-   
-            <Router history={history} >
-                <Wrapper>
-                    <Navbar auth={auth} />
-                    <div className='siteContent'>
-                        <Switch>
-                            
-<<<<<<< HEAD
-                            <Route exact path='/dashboard' component={Dashboard} />
-                            <Route exact path='/AtHome/Inventory' render={(props) => <Inventory auth={auth} {...props} />} />
-                            <Route exact path='/AtHome/Recipes' component={Recipes} />
-                            <Route exact path='/Educate/FoodWaste' component={FoodWaste} />
-                            <Route exact path='/Educate/Hunger' component={Hunger} />
-=======
-                            <Route exact path='/Dashboard' component={Dashboard} />
-                            <Route exact path='/FoodTracker' render={(props) => <FoodTracker auth={auth} {...props} />} />
-                            <Route exact path='/Recipes' component={Recipes} />
->>>>>>> 2933083952a67dbcfc00fda8f80cfb31347cdd32
-                            <Route exact path='/GetInvolved/FoodSupplier' component={FoodSupplier} />
-                            <Route exact path='/GetInvolved/Individual' component={Individual} />
-                            <Route exact path='/GetInvolved/Resources' component={Resources} />
-                            <Route exact path='/' render={(props) => <Home auth={auth} {...props} />} />
-<<<<<<< HEAD
-=======
-                            <Route exact path='/home' render={(props) => <Home auth={auth} {...props} />} />
->>>>>>> 2933083952a67dbcfc00fda8f80cfb31347cdd32
-                            <Route exact path="/profile" render={(props) => <Profile auth={auth} {...props} />} />
-                            <Route path="/callback" render={(props) => {
-                                handleAuthentication(props);
-                                return <Callback {...props} />
-                            }} />
-                            <Route component={NoMatch} />
-                        </Switch>
-                        
-                    </div>
-                    <Footer />
-                </Wrapper>
-            </Router>
-        );
 
-        export default App;
+    <Router history={ history } >
+        <Wrapper>
+            <Navbar auth={ auth } />
+            <div className='siteContent'>
+                <Switch>
+
+                    <Route exact path='/dashboard' component={ Dashboard } />
+                    <Route exact path='/AtHome/Inventory' render={ ( props ) => <Inventory auth={ auth } { ...props } /> } />
+                    <Route exact path='/AtHome/Recipes' component={ Recipes } />
+                    <Route exact path='/Educate/FoodWaste' component={ FoodWaste } />
+                    <Route exact path='/Educate/Hunger' component={ Hunger } />
+                    <Route exact path='/GetInvolved/FoodSupplier' component={ FoodSupplier } />
+                    <Route exact path='/GetInvolved/Individual' component={ Individual } />
+                    <Route exact path='/GetInvolved/Resources' component={ Resources } />
+                    <Route exact path='/' render={ ( props ) => <Home auth={ auth } { ...props } /> } />
+                    <Route exact path="/profile" render={ ( props ) => <Profile auth={ auth } { ...props } /> } />
+                    <Route path="/callback" render={ ( props ) => {
+                        handleAuthentication( props );
+                        return <Callback { ...props } />
+                    } } />
+                    <Route component={ NoMatch } />
+                </Switch>
+
+            </div>
+            <Footer />
+        </Wrapper>
+    </Router>
+);
+
+export default App;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,11 +4,7 @@ import About from '../components/About';
 import Carousel from '../components/Carousel';
 import Features from '../components/Features';
 import Facts from '../components/Facts';
-<<<<<<< HEAD
 import CalltoAction from '../components/CalltoAction';
-=======
-import FoodTracker from '../components/FoodTracker';
->>>>>>> 2933083952a67dbcfc00fda8f80cfb31347cdd32
 import Recipes from '../components/Recipes';
 import { CardWithImage, CardDeck } from '../components/Card';
 import { List, ListItem } from '../components/List';
@@ -17,17 +13,12 @@ export class Home extends Component {
     render() {
         return (
             <div>
-                <Carousel/>
-                <Features/>
-                <About/>
-                <Facts/>
-<<<<<<< HEAD
-                <CalltoAction auth={this.props.auth}/>
-=======
-                <FoodTracker auth={this.props.auth}/>
->>>>>>> 2933083952a67dbcfc00fda8f80cfb31347cdd32
-                <Recipes/>
-                
+                <Carousel />
+                <Features />
+                <About />
+                <Facts />
+                <CalltoAction auth={ this.props.auth } />
+                <Recipes />
             </div>
         )
     };
