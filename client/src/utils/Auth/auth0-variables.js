c77e4ad30b9d98df4a5134ac30dc6f3eabee51b6
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-08-15 13:23:33 -0500

    Update auth0-variables.js

--- client/src/utils/Auth/auth0-variables.js
+++ client/src/utils/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
     domain: process.env.domain || 'leabney.auth0.com',
     clientId: process.env.clientId || 'VcFCURLaQ5Utl16E2Fdvmi1dTuftkmU5',
-    callbackUrl: process.env.callbackURL || 'https://localhost:3000/callback'
+    callbackUrl: process.env.callbackURL || 'https://waste-not-dallas.herokuapp.com/callback'
 }
