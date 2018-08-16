12d7acb67fb724d68ea26595988ffc8fde902968
unknown <leabney@gmail.com>
2018-08-16 17:21:57 -0500

    remove text on dashboard page

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -344,7 +344,7 @@ export class Dashboard extends Component {
                     <h3>My Dashboard</h3>
                     <Link className='add-to-your-items' to="/FoodTracker">Add Food</Link>
                 </div>
-                <h4>My Saved Food</h4>
+                
                 { this.state.saved.slice( 0, this.state.limit )
                     .map( ( saved, index ) => {
                         tableSaved.row.add( {
@@ -392,7 +392,7 @@ export class Dashboard extends Component {
                     }
                     { this.state.recipes.length > 0 && (
                         <React.Fragment>
-                            <h4>My Recipes</h4>
+                            
                             <Link className='add-to-your-items' to="/Recipes">Add Recipes</Link>
                             <br />
                             { this.state.recipes.slice( 0, this.state.recipes.length ).map( ( recipe, index ) => {
