9799c2e65e76da0d98f6109b99a6078f7ee7efd5
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-08-16 12:16:45 -0500

    updating callback URL

--- client/src/utils/Auth/auth0-variables.js
+++ client/src/utils/Auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
     domain: 'leabney.auth0.com',
     clientId: 'VcFCURLaQ5Utl16E2Fdvmi1dTuftkmU5',
-    // callbackUrl: 'https://waste-not-dallas.herokuapp.com/callback'
-    callbackUrl: 'http://localhost:3000/callback'
+    callbackUrl: 'https://waste-not-dallas.herokuapp.com/callback'
+    // callbackUrl: 'http://localhost:3000/callback'
 }
